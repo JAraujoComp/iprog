1455345d304712807e5ac90b980b6f0cb37424a4
JAraujoComp <eu.joaotiago@gmail.com>
2016-10-11 16:52:43 +0100

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,1 +1,51 @@
-#hello
+state = "0"
+#init
+while state == "0":
+    print "\n--------------------------------------"
+    print "---------Welcome to Connect 4---------"
+    print "Type 'play' to play or 'exit' to leave\n"
+    state = raw_input()
+    #to exit:
+    if state == "exit":
+        exit()
+
+while state == "play":
+    #import from text file - player name/winning cond. (0/1)/tamanho da board
+    #number of players:
+    play1 = "" #import
+    play2 = "" #import
+    game = []
+    if play1 == "" or play2 == "":
+        play1 = raw_input("Player 1 name: ")
+        play2 = raw_input("Player 2 name: ")
+    ln = 4 #import
+    col = 5  #import
+    line = ls
+    #possible choosing of simbol
+
+    #init
+    for x in range(line*col):
+        game.append(" ")
+        
+    def change(col)
+    
+
+    mult = 0.
+    turn = 0
+    for x in range(0,line*col-1):
+        if game[x] == " "
+    
+
+    change(int(raw_input(player,"'s turn: "))
+
+
+    if player == play1:
+        player = play2
+    else:
+        player = play1
+
+
+    for x in range(line):
+        for x in range(col):
+            print "hey"
+
